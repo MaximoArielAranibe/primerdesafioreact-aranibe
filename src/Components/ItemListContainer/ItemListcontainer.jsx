742f4df449fe084bd267e506d7d6b08fb4b5e8d9
Maximo Aranibe <darknesswong@gmail.com>
2023-07-21 16:04:24 -0300

    Agregando css...

--- src/Components/ItemListContainer/ItemListcontainer.jsx
+++ src/Components/ItemListContainer/ItemListcontainer.jsx
@@ -3,36 +3,53 @@ import "./index.css";
 const ItemListContainer = (props) => {
   return (
     <main className="item_list_container">
-      <div id="carouselExampleIndicators" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://i.ibb.co/TBm49Vc/whey-protein-mass-spx.png" class="d-block w-30 position-relative translate-middle" alt="..." />
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="..." />
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="..." />
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+      <div id="suplementos-carousel" class="carousel slide">
+        <div class="carousel-inner">
+          <div class="carousel-item active c-item">
+            <img
+              src="https://i.ibb.co/WgZQLyG/whey-protein-ena.png"
+              class="d-block w-100 c-img"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item c-item">
+            <img
+              src="https://i.ibb.co/4ZKDcgK/preentreno-beastblood.png"
+              class="d-block w-100 c-img"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item c-item">
+            <img
+              src="https://i.ibb.co/QKz7qMr/quemador-de-grasa-ena.png"
+              class="d-block w-100 c-img"
+              alt="..."
+            />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#suplementos-carousel"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#suplementos-carousel"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      <hr className="item-hr" />
       <h3 className="item_list_title">Nuestros productos</h3>
-      <section className="main-container">
-        {props.children}
-    </section>
+      <hr className="item-hr" />
+      <section className="main-container">{props.children}</section>
     </main>
   );
 };
